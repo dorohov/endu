21d60d41e1d8a5510a11ff8ff6b8936b1c1165da
Alexander <alexander.zybin@gmail.com>
2016-06-17 20:19:26 +0300

    201606172019

--- src/block/site/diy-selfibot/_b-azbn-common/.document-ready.js
+++ src/block/site/diy-selfibot/_b-azbn-common/.document-ready.js
@@ -12,6 +12,16 @@ $(function() {
 		
 	});
 	
+	$(document.body).on('click.azbn', '.b-azbn-diy-selfibot-container .youtube-btn2', {}, function(event){
+		event.preventDefault();
+		
+		var btn = $(this);
+		var uid = btn.attr('data-youtube-uid');//href="https://www.youtube.com/embed/qo-35n1BBms"
+		
+		$('.b-azbn-modal-block .youtube-modal-video').attr('src', 'https://www.youtube.com/embed/' + uid);
+		
+	});
+	
 });
 
 $(function() {
---
+++ src/block/site/diy-selfibot/_b-azbn-common/.window-resize.js
@@ -0,0 +1,20 @@
+$(function() {
+
+	if(		$('html').hasClass('desktop')
+			&&
+			($(window).outerHeight(true) < 768)
+			&&
+			($(window).outerWidth(true) / $(window).outerHeight(true) > 1.85)
+		) {
+		
+		if($('body').hasClass('xs-h')) {
+			
+		} else {
+			$('body').addClass('xs-h');
+		}
+		
+	} else {
+		$('body').removeClass('xs-h');
+	}
+
+});
