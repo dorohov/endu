7c6ee4caee8e2c6b5361e4250ab86fd84045361b
Alexander <alexander.zybin@gmail.com>
2016-06-27 12:40:18 +0300

    201606271240

--- js/document-ready.js
+++ js/document-ready.js
@@ -191,7 +191,7 @@ $(window).on('resize',function(event){
 	
 	$(function(){var w={xs:{min:0,max:768},sm:{min:767,max:992},md:{min:991,max:1200},lg:{min:1199,max:1e4}},s="window-width",d=$(window).outerWidth(!0),i=$("body.fecss").eq(0);d<w.xs.max&&(i.hasClass("window-width-sm")&&i.removeClass("window-width-sm"),i.hasClass("window-width-md")&&i.removeClass("window-width-md"),i.hasClass("window-width-lg")&&i.removeClass("window-width-lg"),s="window-width-xs"),d>w.sm.min&&d<w.sm.max&&(i.hasClass("window-width-xs")&&i.removeClass("window-width-xs"),i.hasClass("window-width-md")&&i.removeClass("window-width-md"),i.hasClass("window-width-lg")&&i.removeClass("window-width-lg"),s="window-width-sm"),d>w.md.min&&d<w.md.max&&(i.hasClass("window-width-xs")&&i.removeClass("window-width-xs"),i.hasClass("window-width-sm")&&i.removeClass("window-width-sm"),i.hasClass("window-width-lg")&&i.removeClass("window-width-lg"),s="window-width-md"),d>w.lg.min&&(i.hasClass("window-width-xs")&&i.removeClass("window-width-xs"),i.hasClass("window-width-sm")&&i.removeClass("window-width-sm"),i.hasClass("window-width-md")&&i.removeClass("window-width-md"),s="window-width-lg"),$("body.fecss").eq(0).addClass(s)});
 
-$(function(){$("html").hasClass("desktop")&&$(window).outerHeight(!0)<768&&$(window).outerWidth(!0)/$(window).outerHeight(!0)>1.85?$("body").hasClass("xs-h")||$("body").addClass("xs-h"):$("body").removeClass("xs-h")});
+$(function(){$("html").hasClass("desktop")&&$(window).outerHeight(!0)<768?$("body").hasClass("xs-h")||$("body").addClass("xs-h"):$("body").removeClass("xs-h")});
 
 	
 	
--- src/block/site/diy-selfibot/_b-azbn-common/.window-resize.js
+++ src/block/site/diy-selfibot/_b-azbn-common/.window-resize.js
@@ -3,8 +3,8 @@ $(function() {
 	if(		$('html').hasClass('desktop')
 			&&
 			($(window).outerHeight(true) < 768)
-			&&
-			($(window).outerWidth(true) / $(window).outerHeight(true) > 1.85)
+			//&&
+			//($(window).outerWidth(true) / $(window).outerHeight(true) > 1.85)
 		) {
 		
 		if($('body').hasClass('xs-h')) {
