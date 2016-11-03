9ebf657805975fe0d3ca96d2b9e78ccf39f870db
caezar47 <iss47@ya.ru>
2016-11-03 08:50:46 +0300

    fix adaptive-block

--- src/_/concat.window-scroll.js
+++ src/_/concat.window-scroll.js
@@ -1,1 +1,1 @@
-$(function(){var a=$(document).scrollTop(),o=$(".goto-top");o.hasClass("active")?a<450&&o.removeClass("active"):a>450&&o.addClass("active")});
+$(function(){var a=$(document).scrollTop(),o=$(".goto-top");o.hasClass("active")?450>a&&o.removeClass("active"):a>450&&o.addClass("active")});
